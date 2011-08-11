a3ce3d3fd9b783f0a2cb265202244457a126276f
James Campos <james.r.campos@gmail.com>
2011-08-10 18:31:25 -0700

    remove stable

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -7,7 +7,7 @@
 // @license        MIT; http://en.wikipedia.org/wiki/Mit_license
 // @include        http://boards.4chan.org/*
 // @include        http://sys.4chan.org/*
-// @updateURL      https://github.com/aeosynth/4chan-x/raw/stable/4chan_x.user.js
+// @updateURL      https://github.com/aeosynth/4chan-x/raw/master/4chan_x.user.js
 // ==/UserScript==
 
 /* LICENSE
