9b57b62ed7463c4b78feca067db88328c5caa8ca
James Campos <james.r.campos@gmail.com>
2011-08-15 21:44:58 -0700

    changelog, remove @version

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -2,7 +2,6 @@
 // @name           4chan x
 // @namespace      aeosynth
 // @description    Adds various features.
-// @version        11.8.11.1
 // @copyright      2009-2011 James Campos <james.r.campos@gmail.com>
 // @license        MIT; http://en.wikipedia.org/wiki/Mit_license
 // @include        http://boards.4chan.org/*
