21d02ce569aa7cc7c412dc784eae4d085bf4348c
James Campos <james.r.campos@gmail.com>
2011-10-28 20:50:18 -0700

    there are two "GitHub" links

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -1199,7 +1199,7 @@
   <div id=optionsbar>\
     <div id=credits>\
       <a href=http://aeosynth.github.com/4chan-x/>4chan X</a>\
-      | <a href=https://github.com/aeosynth/4chan-x/issues>GitHub</a>\
+      | <a href=https://github.com/aeosynth/4chan-x/issues>Issues</a>\
       | <a href=https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=2DBVZBUAM4DHC&lc=US&item_name=Aeosynth&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted>Donate</a>\
     </div>\
     <div>\
