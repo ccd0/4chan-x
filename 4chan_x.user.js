cd8109c986e4e2668f6d7d04e9d26fd9c71ea720
James Campos <james.r.campos@gmail.com>
2011-10-30 12:39:09 -0700

    fix #357

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -1226,8 +1226,8 @@
   <div id=optionsbar>\
     <div id=credits>\
       <a href=http://aeosynth.github.com/4chan-x/>4chan X</a>\
-      | <a href=https://github.com/aeosynth/4chan-x/issues>Issues</a>\
-      | <a href=https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=2DBVZBUAM4DHC&lc=US&item_name=Aeosynth&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted>Donate</a>\
+      | <a target=_blank href=https://github.com/aeosynth/4chan-x/issues>Issues</a>\
+      | <a target=_blank href=https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=2DBVZBUAM4DHC&lc=US&item_name=Aeosynth&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted>Donate</a>\
     </div>\
     <div>\
       <label for=main_tab>Main</label>\
--- script.coffee
+++ script.coffee
@@ -870,8 +870,8 @@ options =
   <div id=optionsbar>
     <div id=credits>
       <a href=http://aeosynth.github.com/4chan-x/>4chan X</a>
-      | <a href=https://github.com/aeosynth/4chan-x/issues>Issues</a>
-      | <a href=https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=2DBVZBUAM4DHC&lc=US&item_name=Aeosynth&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted>Donate</a>
+      | <a target=_blank href=https://github.com/aeosynth/4chan-x/issues>Issues</a>
+      | <a target=_blank href=https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=2DBVZBUAM4DHC&lc=US&item_name=Aeosynth&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted>Donate</a>
     </div>
     <div>
       <label for=main_tab>Main</label>
