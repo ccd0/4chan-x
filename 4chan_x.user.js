80bf28e6732abae13f857bd4a8f5c18a1abd338d
James Campos <james.r.campos@gmail.com>
2011-11-11 09:36:27 -0800

    rm archive.installgentoo.net

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -2739,16 +2739,12 @@
       case 'lit':
         url = "http://archive.gentoomen.org/cgi-board.pl/" + g.BOARD + "/thread/" + g.THREAD_ID;
         break;
-      case 'diy':
-      case 'g':
-      case 'sci':
-        url = "http://archive.installgentoo.net/" + g.BOARD + "/thread/" + g.THREAD_ID;
-        break;
       case '3':
       case 'adv':
       case 'an':
       case 'ck':
       case 'co':
+      case 'diy':
       case 'fa':
       case 'fit':
       case 'int':
