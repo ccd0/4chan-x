128f66c9b12a4df2c8b9bff305cf4615da715870
Nicolas Stepien <stepien.nicolas@gmail.com>
2012-04-30 05:02:00 +0200

    Fix mimetype checking. Close #400
    Conflicts:
    
    	4chan_x.user.js
    	script.coffee

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -1841,7 +1841,7 @@
         });
         ta.style.cssText = $.get('QR.size', '');
       }
-      mimeTypes = $('.rules').firstChild.textContent.match(/: (.+) /)[1].toLowerCase().replace(/\w+/g, function(type) {
+      mimeTypes = $('.rules').firstElementChild.textContent.match(/: (.+)/)[1].toLowerCase().replace(/\w+/g, function(type) {
         switch (type) {
           case 'jpg':
             return 'image/jpeg';
