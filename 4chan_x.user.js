6771acd38280bcf9a91d7b6792dc8a9ce78c8d40
James Campos <james.r.campos@gmail.com>
2012-05-16 03:59:13 -0700

    markdown

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -73,7 +73,7 @@
  */
 
 (function() {
-  var $, $$, Anonymize, AutoGif, Conf, Config, ExpandComment, ExpandThread, Favicon, FileInfo, Filter, GetTitle, ImageExpand, ImageHover, Keybinds, Main, Nav, Options, Prefetch, QR, QuoteBacklink, QuoteCT, QuoteInline, QuoteOP, QuotePreview, QuoteThreading, Quotify, Redirect, ReplyHiding, ReportButton, RevealSpoilers, Sauce, StrikethroughQuotes, ThreadHiding, ThreadStats, Time, TitlePost, UI, Unread, Updater, Watcher, d, g, log, _base;
+  var $, $$, Anonymize, AutoGif, Conf, Config, ExpandComment, ExpandThread, Favicon, FileInfo, Filter, GetTitle, ImageExpand, ImageHover, Keybinds, Main, Markdown, Nav, Options, Prefetch, QR, QuoteBacklink, QuoteCT, QuoteInline, QuoteOP, QuotePreview, QuoteThreading, Quotify, Redirect, ReplyHiding, ReportButton, RevealSpoilers, Sauce, StrikethroughQuotes, ThreadHiding, ThreadStats, Time, TitlePost, UI, Unread, Updater, Watcher, d, g, log, _base;
 
   Config = {
     main: {
@@ -126,7 +126,8 @@
         'Remember QR size': [false, 'Remember the size of the Quick reply (Firefox only).'],
         'Remember Subject': [false, 'Remember the subject field, instead of resetting after posting.'],
         'Remember Spoiler': [false, 'Remember the spoiler state, instead of resetting after posting.'],
-        'Hide Original Post Form': [true, 'Replace the normal post form with a shortcut to open the QR.']
+        'Hide Original Post Form': [true, 'Replace the normal post form with a shortcut to open the QR.'],
+        'Markdown': [false, 'Code, italic, bold, italic bold - `, *, **, ***, respectively. _ can be used instead of *']
       },
       Quoting: {
         'Quote Backlinks': [true, 'Add quote backlinks'],
@@ -499,6 +500,114 @@
       root = d.body;
     }
     return Array.prototype.slice.call(root.querySelectorAll(selector));
+  };
+
+  Markdown = {
+    format: function(text) {
+      var pattern, tag, tag_patterns;
+      tag_patterns = {
+        bi: /(\*\*\*|___)(?=\S)([^\r\n]*?\S)\1/g,
+        b: /(\*\*|__)(?=\S)([^\r\n]*?\S)\1/g,
+        i: /(\*|_)(?=\S)([^\r\n]*?\S)\1/g,
+        code: /(`)(?=\S)([^\r\n]*?\S)\1/g
+      };
+      for (tag in tag_patterns) {
+        pattern = tag_patterns[tag];
+        text = text.replace(pattern, Markdown.unicode_convert);
+      }
+      return text;
+    },
+    unicode_convert: function(str, tag, inner) {
+      var c, charcode, charcodes, codepoints, codes, fmt, i, unicode_text;
+      if (tag === "_" || tag === "*") {
+        fmt = "i";
+      } else if (tag === "__" || tag === "**") {
+        fmt = "b";
+      } else if (tag === "***" || tag === "___") {
+        fmt = "bi";
+      } else {
+        if (tag === "`" || tag === "```") {
+          fmt = "code";
+        }
+      }
+      codepoints = {
+        b: [0x1D7CE, 0x1D400, 0x1D41A],
+        i: [0x1D7F6, 0x1D434, 0x1D44E],
+        bi: [0x1D7CE, 0x1D468, 0x1D482],
+        code: [0x1D7F6, 0x1D670, 0x1D68A]
+      };
+      charcodes = (function() {
+        var _i, _len, _results;
+        _results = [];
+        for (i = _i = 0, _len = inner.length; _i < _len; i = ++_i) {
+          c = inner[i];
+          _results.push(inner.charCodeAt(i));
+        }
+        return _results;
+      })();
+      codes = (function() {
+        var _i, _len, _results;
+        _results = [];
+        for (_i = 0, _len = charcodes.length; _i < _len; _i++) {
+          charcode = charcodes[_i];
+          if (charcode >= 48 && charcode <= 57) {
+            _results.push(charcode - 48 + codepoints[fmt][0]);
+          } else if (charcode >= 65 && charcode <= 90) {
+            _results.push(charcode - 65 + codepoints[fmt][1]);
+          } else if (charcode >= 97 && charcode <= 122) {
+            if (charcode === 104 && tag === "i") {
+              _results.push(0x210E);
+            } else {
+              _results.push(charcode - 97 + codepoints[fmt][2]);
+            }
+          } else {
+            _results.push(charcode);
+          }
+        }
+        return _results;
+      })();
+      unicode_text = codes.map(Markdown.ucs2_encode).join("");
+      if (tag === "code") {
+        unicode_text = unicode_text.replace(/\x20/g, "\xA0");
+      }
+      return unicode_text;
+    },
+    ucs2_encode: function(value) {
+      /*
+          From Punycode.js: https://github.com/bestiejs/punycode.js
+      
+          Copyright Mathias Bynens <http://mathiasbynens.be/>
+      
+          Permission is hereby granted, free of charge, to any person obtaining
+          a copy of this software and associated documentation files (the
+          "Software"), to deal in the Software without restriction, including
+          without limitation the rights to use, copy, modify, merge, publish,
+          distribute, sublicense, and/or sell copies of the Software, and to
+          permit persons to whom the Software is furnished to do so, subject to
+          the following conditions:
+      
+          The above copyright notice and this permission notice shall be
+          included in all copies or substantial portions of the Software.
+      
+          THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+          EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF`
+          MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+          NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+          LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+          OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+          WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+      */
+
+      var output;
+      output = "";
+      if (value > 0xFFFF) {
+        value -= 0x10000;
+        output += String.fromCharCode(value >>> 10 & 0x3FF | 0xD800);
+        value = 0xDC00 | value & 0x3FF;
+      }
+      output += String.fromCharCode(value);
+      return output;
+    }
   };
 
   Filter = {
@@ -2023,7 +2132,7 @@
         name: reply.name,
         email: reply.email,
         sub: reply.sub,
-        com: reply.com,
+        com: Conf['Markdown'] ? Markdown.format(reply.com) : reply.com,
         upfile: reply.file,
         spoiler: reply.spoiler,
         mode: 'regist',
--- script.coffee
+++ script.coffee
@@ -46,6 +46,7 @@ Config =
       'Remember Subject':             [false, 'Remember the subject field, instead of resetting after posting.']
       'Remember Spoiler':             [false, 'Remember the spoiler state, instead of resetting after posting.']
       'Hide Original Post Form':      [true,  'Replace the normal post form with a shortcut to open the QR.']
+      'Markdown':                     [false, 'Code, italic, bold, italic bold - `, *, **, ***, respectively. _ can be used instead of *']
     Quoting:
       'Quote Backlinks':              [true,  'Add quote backlinks']
       'OP Backlinks':                 [false, 'Add backlinks to the OP']
@@ -379,6 +380,90 @@ $.extend $,
 
 $$ = (selector, root=d.body) ->
   Array::slice.call root.querySelectorAll selector
+
+Markdown =
+  format: (text) ->
+    tag_patterns =
+      bi: /(\*\*\*|___)(?=\S)([^\r\n]*?\S)\1/g
+      b: /(\*\*|__)(?=\S)([^\r\n]*?\S)\1/g
+      i: /(\*|_)(?=\S)([^\r\n]*?\S)\1/g
+      code: /(`)(?=\S)([^\r\n]*?\S)\1/g
+
+    for tag, pattern of tag_patterns
+      text = text.replace pattern, Markdown.unicode_convert
+    text
+
+  unicode_convert: (str, tag, inner) ->
+    if tag is "_" or tag is "*"
+      fmt = "i"
+    else if tag is "__" or tag is "**"
+      fmt = "b"
+    else if tag is "***" or tag is "___"
+      fmt = "bi"
+    else fmt = "code"  if tag is "`" or tag is "```"
+
+    #Unicode codepoints for the characters '0', 'A', and 'a'
+    #http://en.wikipedia.org/wiki/Mathematical_Alphanumeric_Symbols
+    codepoints =
+      b:    [ 0x1D7CE, 0x1D400, 0x1D41A ] #MATHEMATICAL BOLD
+      i:    [ 0x1D7F6, 0x1D434, 0x1D44E ] #MATHEMATICAL ITALIC
+      bi:   [ 0x1D7CE, 0x1D468, 0x1D482 ] #MATHEMATICAL BOLD ITALIC
+      code: [ 0x1D7F6, 0x1D670, 0x1D68A ] #MATHEMATICAL MONOSPACE
+
+    charcodes = (inner.charCodeAt i for c, i in inner)
+
+    codes = for charcode in charcodes
+      if charcode >= 48 and charcode <= 57
+        charcode - 48 + codepoints[fmt][0]
+      else if charcode >= 65 and charcode <= 90
+        charcode - 65 + codepoints[fmt][1]
+      else if charcode >= 97 and charcode <= 122
+        if charcode is 104 and tag is "i"
+          #http://blogs.msdn.com/b/michkap/archive/2006/04/21/580328.aspx
+          #mathematical small h -> planck constant
+          0x210E
+        else
+          charcode - 97 + codepoints[fmt][2]
+      else
+        charcode
+
+    unicode_text = codes.map(Markdown.ucs2_encode).join ""
+    unicode_text = unicode_text.replace(/\x20/g, "\xA0")  if tag is "code"
+    unicode_text
+
+  ucs2_encode: (value) ->
+    #translates Unicode codepoint integers directly into text. Javascript does this in an ugly fashion by default.
+    ###
+    From Punycode.js: https://github.com/bestiejs/punycode.js
+
+    Copyright Mathias Bynens <http://mathiasbynens.be/>
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF`
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    ###
+    output = ""
+    if value > 0xFFFF
+      value -= 0x10000
+      output += String.fromCharCode value >>> 10 & 0x3FF | 0xD800
+      value = 0xDC00 | value & 0x3FF
+    output += String.fromCharCode value
+    output
 
 Filter =
   filters: {}
@@ -1537,7 +1622,7 @@ QR =
       name:    reply.name
       email:   reply.email
       sub:     reply.sub
-      com:     reply.com
+      com:     if Conf['Markdown'] then Markdown.format reply.com else reply.com
       upfile:  reply.file
       spoiler: reply.spoiler
       mode:    'regist'
