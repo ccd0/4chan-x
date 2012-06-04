bdca125758e26c4ad54496ff67fc42959408165f
James Campos <james.r.campos@gmail.com>
2012-06-03 18:53:21 -0700

    derp

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -1055,7 +1055,7 @@
     },
     keydown: function(e) {
       var key, link, o, target, thread;
-      if (!(key = Kebinds.keyCode(e))) {
+      if (!(key = Keybinds.keyCode(e))) {
         return;
       }
       target = e.target;
