f474d49c38b99ef68efc953901eaa600b73b12be
Nicolas Stepien <stepien.nicolas@gmail.com>
2012-09-18 19:50:23 +0200

    Release 2.35.1.

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           4chan x
-// @version        2.35.0
+// @version        2.35.1
 // @namespace      aeosynth
 // @description    Adds various features.
 // @copyright      2009-2011 James Campos <james.r.campos@gmail.com>
@@ -27,7 +27,7 @@
  * Copyright (c) 2009-2011 James Campos <james.r.campos@gmail.com>
  * Copyright (c) 2012 Nicolas Stepien <stepien.nicolas@gmail.com>
  * http://mayhemydg.github.com/4chan-x/
- * 4chan X 2.35.0
+ * 4chan X 2.35.1
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -5356,7 +5356,7 @@
       return $.globalEval(("(" + code + ")()").replace('_id_', bq.id));
     },
     namespace: '4chan_x.',
-    version: '2.35.0',
+    version: '2.35.1',
     callbacks: [],
     css: '\
 /* dialog styling */\
--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-postMessage({version:'2.35.0'},'*')
+postMessage({version:'2.35.1'},'*')
