3e83a11b099358ea869335f2e1e5a22b852b5f39
Nicolas Stepien <stepien.nicolas@gmail.com>
2012-09-20 01:11:25 +0200

    Fix #754

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -5978,7 +5978,7 @@ body.unscroll {\
   max-width: 500px;\
 }\
 .qphl {\
-  outline: 2px solid rgba(216, 94, 49, .7);\
+  box-shadow: 0 0 0 2px rgba(216, 94, 49, .7);\
 }\
 .quotelink.deadlink {\
   text-decoration: underline !important;\
@@ -6006,10 +6006,18 @@ div.opContainer {\
   display: block !important;\
 }\
 .opContainer.filter_highlight {\
-  box-shadow: inset 5px 0 rgba(255,0,0,0.5);\
+  box-shadow: inset 5px 0 rgba(255, 0, 0, .5);\
+}\
+.opContainer.filter_highlight.qphl {\
+  box-shadow: inset 5px 0 rgba(255, 0, 0, .5),\
+              0 0 0 2px rgba(216, 94, 49, .7);\
 }\
 .filter_highlight > .reply {\
-  box-shadow: -5px 0 rgba(255,0,0,0.5);\
+  box-shadow: -5px 0 rgba(255, 0, 0, .5);\
+}\
+.filter_highlight > .reply.qphl {\
+  box-shadow: -5px 0 rgba(255, 0, 0, .5),\
+              0 0 0 2px rgba(216, 94, 49, .7)\
 }\
 .filtered,\
 .quotelink.filtered {\
