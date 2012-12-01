96a4ba8024a94ffee0b94a276e5b677003c2f57a
Zixaphir <zixaphirmoxphar@gmail.com>
2012-12-01 01:47:08 -0700

    Release appchan x v1.0.17.

--- appchan_x.user.js
+++ appchan_x.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                appchan x
 // @namespace           zixaphir
-// @version             1.0.16
+// @version             1.0.17
 // @description         Cross-browser userscript for maximum lurking on 4chan.
 // @copyright           2012 Zixaphir <zixaphirmoxphar@gmail.com>
 // @copyright           2009-2011 James Campos <james.r.campos@gmail.com>
@@ -19,7 +19,7 @@
 // ==/UserScript==
 
 /*
- * appchan x - Version 1.0.16 - 2012-12-01
+ * appchan x - Version 1.0.17 - 2012-12-01
  *
  * Licensed under the MIT license.
  * https://github.com/zixaphir/appchan-x/blob/master/LICENSE
@@ -11036,7 +11036,7 @@
       return $.globalEval(("" + code).replace('_id_', bq.id));
     },
     namespace: 'appchan_x.',
-    version: '1.0.16',
+    version: '1.0.17',
     callbacks: []
   };
 
--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-postMessage({version:'1.0.16'},'*')
+postMessage({version:'1.0.17'},'*')
