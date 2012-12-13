e326d923329560a56095dff29117dd50e0f4de47
noface <noface@outlook.com>
2012-12-13 17:39:12 +0100

    Anonymize on stubs.

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -1204,7 +1204,7 @@
       });
       a = stub.firstChild;
       $.on(a, 'click', ReplyHiding.toggle);
-      $.add(a, $.tn($('.desktop > .nameBlock', el).textContent));
+      $.add(a, $.tn(Conf['Anonymize'] ? 'Anonymize' : $('.desktop > .nameBlock', el).textContent));
       if (Conf['Menu']) {
         menuButton = Menu.a.cloneNode(true);
         $.on(menuButton, 'click', Menu.toggle);
