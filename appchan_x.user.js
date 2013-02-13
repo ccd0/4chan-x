8a63ddd1727f5d6e8ed0fa525bcb296e252e8ab2
Zixaphir <zixaphirmoxphar@gmail.com>
2013-02-12 18:46:29 -0700

    Crude icon. Close #132

--- appchan_x.user.js
+++ appchan_x.user.js
@@ -15,7 +15,7 @@
 // @run-at              document-start
 // @updateURL           https://github.com/zixaphir/appchan-x/raw/stable/appchan_x.meta.js
 // @downloadURL         https://github.com/zixaphir/appchan-x/raw/stable/appchan_x.user.js
-// @icon                https://github.com/zixaphir/appchan-x/raw/stable/img/icon.gif
+// @icon                data:image/gif;base64,R0lGODlhYAAQAJEAAGbMM////wAAAP///yH5BAEAAAMALAAAAABgABAAAAL8nI+py+0Po5y02ruEFmh7PnxbJ3KecR4pqoWr2iajIgR2AIb3bu43WPPhdENiLmg7Jn25EODZFDwBTSIPJRwir1tuElvshrNf1RR4lv2QPZzmG9zA16c4uO3+WeVl55Qq9RdVZlc4p3VYdcfXBpSo0ufyJ1gVx2j0huhmWKSW1Xhpd0czSZVBhucl1Hj6acVkBNnZIcggdgh7y0qTFIoaOypTqoiZCTpjiRwp66jp8RhsVmrq2bzbsbz3AcxpDfztBxUIVT0qij0LtsoN3c1MSx5eSQiNTqzL7mzed+4khfZPBrIRL1jEoFHCRYw6A1usKGiiQwgMFCtalFAAADs=
 // ==/UserScript==
 
 /*
