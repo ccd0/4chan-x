f2041ab72e147d67a36182a27c0b4c6bd97204cd
Jordan Bates <saudrapsmann@gmail.com>
2013-04-25 15:31:20 -0700

    Add old v2 version temporarily to point to install page

---
+++ 4chan_x.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name                4chan x
+// @namespace           aeosynth
+// @version             1.0.11
+// @description         Cross-browser userscript for maximum lurking on 4chan.
+// @copyright           2013-2013 Zixaphir <zixaphirmoxphar@gmail.com>
+// @copyright           2013-2013 Jordan Bates <saudrapsmann@gmail.com>
+// @copyright           2009-2011 James Campos <james.r.campos@gmail.com>
+// @copyright           2012-2013 Nicolas Stepien <stepien.nicolas@gmail.com>
+// @license             MIT; http://en.wikipedia.org/wiki/Mit_license
+// @match               *://*.4chan.org/*
+// @grant               GM_getValue
+// @grant               GM_setValue
+// @grant               GM_deleteValue
+// @grant               GM_openInTab
+// @run-at              document-start
+// @updateURL           https://github.com/seaweedchan/4chan-x/raw/stable/4chan_x.meta.js
+// @downloadURL         https://github.com/seaweedchan/4chan-x/raw/stable/4chan_x.user.js
+// @icon                data:image/gif;base64,R0lGODlhYAAQAJEAAGbMM////wAAAP///yH5BAEAAAMALAAAAABgABAAAAL8nI+py+0Po5y02ruEFmh7PnxbJ3KecR4pqoWr2iajIgR2AIb3bu43WPPhdENiLmg7Jn25EODZFDwBTSIPJRwir1tuElvshrNf1RR4lv2QPZzmG9zA16c4uO3+WeVl55Qq9RdVZlc4p3VYdcfXBpSo0ufyJ1gVx2j0huhmWKSW1Xhpd0czSZVBhucl1Hj6acVkBNnZIcggdgh7y0qTFIoaOypTqoiZCTpjiRwp66jp8RhsVmrq2bzbsbz3AcxpDfztBxUIVT0qij0LtsoN3c1MSx5eSQiNTqzL7mzed+4khfZPBrIRL1jEoFHCRYw6A1usKGiiQwgMFCtalFAAADs=
+// ==/UserScript==
