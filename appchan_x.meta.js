0fbb87dd470d1928c66e46b8b2f7799f57aa87f4
Zixaphir <zixaphirmoxphar@gmail.com>
2013-05-07 23:04:35 -0700

    Add a dummy appchan_x.user.js for updates.

---
+++ appchan_x.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name                appchan x
+// @namespace           zixaphir
+// @version             1.2.7
+// @description         Cross-browser userscript for maximum lurking on 4chan.
+// @copyright           2013 Zixaphir <zixaphirmoxphar@gmail.com>
+// @copyright           2009-2011 James Campos <james.r.campos@gmail.com>
+// @copyright           2013 Nicolas Stepien <stepien.nicolas@gmail.com>
+// @license             MIT; http://en.wikipedia.org/wiki/Mit_license
+// @match               *://*.4chan.org/*
+// @grant               GM_getValue
+// @grant               GM_setValue
+// @grant               GM_deleteValue
+// @grant               GM_openInTab
+// @run-at              document-start
+// @icon                data:image/gif;base64,R0lGODlhIAAgAKIAAAQCBARmBHy6VKTShP///////wAAAAAAACH5BAEAAAUALAAAAAAgACAAAAO6WLrc/jDKWYK9VF3cts2Vx1hDaX4RaZaouqKQewZjINwCS3dBiQuwheWnc6h+weFPAGg6n88lcKf8Qa9OadLTJHi/4KZnV9t0wWixaHK+epsgRhv6BsQXZ/QXfi/k9QR8d3NPdX1+AHWFgXZxFnMKc0ESj4mMkZZqFCpXmE9FKT0DnYifA5NCokhkqUSnrBqqOKhVOa9GNku0uau4WrAhv75TN7vFucbEU8NKqLHPodAxVMA10ofY2REJADs=
+// ==/UserScript==
