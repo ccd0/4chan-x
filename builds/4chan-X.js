17755b435b785802bda525116c3c9dd5e1c44ca1
Zixaphir <zixaphirmoxphar@gmail.com>
2013-05-10 15:56:14 -0700

    And this is why you should do you bugtesting on the source Not the compiled output.

--- builds/4chan-X.js
+++ builds/4chan-X.js
@@ -7257,8 +7257,8 @@
           continue;
         }
         count++;
-        node = Build.postFromObject(postObject, ThreadUpdater.thread.board);
-        posts.push(new Post(node, ThreadUpdater.thread, ThreadUpdater.thread.board.ID));
+        node = Build.postFromObject(postObject, ThreadUpdater.thread.board.ID);
+        posts.push(new Post(node, ThreadUpdater.thread, ThreadUpdater.thread.board));
       }
       deletedPosts = [];
       deletedFiles = [];
