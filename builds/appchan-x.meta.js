5900490e631ebf1c6ec4ee045e6fab11b47b7209
Zixaphir <zixaphirmoxphar@gmail.com>
2013-10-13 17:59:58 -0700

    Release appchan x v2.4.1.

--- builds/appchan-x.meta.js
+++ builds/appchan-x.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         appchan x
-// @version      2.4.0
+// @version      2.4.1
 // @minGMVer     1.13
 // @minFFVer     22
 // @namespace    zixaphir
--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-postMessage({version:'2.4.0'},'*')
+postMessage({version:'2.4.1'},'*')
