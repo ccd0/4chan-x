75b7804eda353dcf3960d9a9b1127fbc6bba72d9
Zixaphir <zixaphirmoxphar@gmail.com>
2013-12-16 20:07:27 -0700

    Fix and recomprehense the setInterval function

--- builds/crx/script.js
+++ builds/crx/script.js
@@ -8995,15 +8995,19 @@
       }
     },
     setInterval: function() {
-      var cur, i, j;
+      var cur, i, j, limit;
 
-      i = ThreadUpdater.interval;
-      j = (cur = ThreadUpdater.outdateCount < 10) ? cur : 10;
-      if (!d.hidden) {
-        j = j < 7 ? j : 7;
+      i = ThreadUpdater.interval + 1;
+      if (Conf['Optional Increase']) {
+        cur = ThreadUpdater.outdateCount || 1;
+        limit = d.hidden ? 7 : 10;
+        j = cur <= limit ? cur : limit;
+        cur = ((i * 0.1).floor() || 1) * j * j;
+        ThreadUpdater.seconds = cur > i ? cur <= 300 ? cur : 300 : i;
+      } else {
+        ThreadUpdater.seconds = i;
       }
-      ThreadUpdater.seconds = Conf['Optional Increase'] ? (cur = [0, 5, 10, 15, 20, 30, 60, 90, 120, 240, 300][j] > i) ? cur : i : i;
-      ThreadUpdater.set('timer', ThreadUpdater.seconds++);
+      ThreadUpdater.set('timer', ThreadUpdater.seconds);
       return ThreadUpdater.count(true);
     },
     intervalShortcut: function() {
