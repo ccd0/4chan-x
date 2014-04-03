f53752c20cdfd438145d1ce1e18d430c1e6732be
ccd0 <admin@containerchan.org>
2014-04-03 15:59:40 -0700

    use gh-pages for updates

--- builds/4chan-X.user.js
+++ builds/4chan-X.user.js
@@ -18,8 +18,8 @@
 // @grant        GM_openInTab
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
-// @updateURL 	 https://github.com/ccd0/4chan-x/raw/stable/builds/4chan-X.meta.js
-// @downloadURL  https://github.com/ccd0/4chan-x/raw/stable/builds/4chan-X.user.js
+// @updateURL 	 https://ccd0.github.io/4chan-x/builds/4chan-X.meta.js
+// @downloadURL  https://ccd0.github.io/4chan-x/builds/4chan-X.user.js
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
 // ==/UserScript==
 
