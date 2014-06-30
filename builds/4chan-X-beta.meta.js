22b0aa5568c7489759bfd1e4a2c8d4eb88394a79
ccd0 <admin@containerchan.org>
2014-06-30 04:35:01 -0700

    Move 4chan X v1.8.2 to beta channel.

---
+++ builds/4chan-X-beta.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         4chan X
+// @version      1.8.2
+// @minGMVer     1.14
+// @minFFVer     26
+// @namespace    4chan-X
+// @description  Cross-browser userscript for maximum lurking on 4chan.
+// @license      MIT; https://github.com/ccd0/4chan-x/blob/master/LICENSE 
+// @match        *://boards.4chan.org/*
+// @match        *://sys.4chan.org/*
+// @match        *://a.4cdn.org/*
+// @match        *://i.4cdn.org/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_openInTab
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @updateURL 	 https://ccd0.github.io/4chan-x/builds/4chan-X-beta.meta.js
+// @downloadURL  https://ccd0.github.io/4chan-x/builds/4chan-X-beta.user.js
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
+// ==/UserScript==
