7841d40cf976947373a60298ba8c03a0327a1509
ccd0 <admin@containerchan.org>
2015-06-21 18:24:00 -0700

    Move 4chan X v1.11.0.9 to stable channel.

--- builds/4chan-X.meta.js
+++ builds/4chan-X.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan X
-// @version      1.10.14.4
+// @version      1.11.0.9
 // @minGMVer     1.14
 // @minFFVer     26
 // @namespace    4chan-X
@@ -14,7 +14,8 @@
 // @match        *://www.4chan.org/feedback
 // @match        https://www.google.com/recaptcha/api2/anchor?k=6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc*
 // @match        https://www.google.com/recaptcha/api2/frame?*&k=6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc*
-// @match        *://www.google.com/recaptcha/api/fallback?k=6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc
+// @match        *://www.google.com/recaptcha/api/fallback?k=6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc*
+// @match        *://www.google.com/recaptcha/api/noscript?k=6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc*
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_deleteValue
