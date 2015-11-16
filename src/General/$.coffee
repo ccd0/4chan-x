ccd0/4chan-x

commit 8aa780cc33333e543b741f878098c3b2b142b7cd

Make our AJAX requests work in Maxthon. #593

src/General/$.coffee
@@ -67,8 +67,14 @@ $.ajax = do ->
       r.setRequestHeader 'If-Modified-Since', lastModified[whenModified][url] if lastModified[whenModified]?[url]?
       $.on r, 'load', -> (lastModified[whenModified] or= {})[url] = r.getResponseHeader 'Last-Modified'
     if /\.json$/.test url
-      r.responseType = 'json'
+      options.responseType ?= 'json'
     $.extend r, options
+    # XXX https://code.google.com/p/chromium/issues/detail?id=119256 (Maxthon is still on Chromium 30)
+    if options.responseType is 'json' and r.responseType isnt 'json' and delete r.response
+      Object.defineProperty r, 'response',
+        configurable: true
+        enumerable:   true
+        get: -> return JSON.parse r.responseText
     $.extend r.upload, upCallbacks
     r.send form
     r

commit 0575eb2a7943c3f9b4aeaed165650c5339c9a6fa

Add default for options in $.ajax.
Fixes bugs caused by 8aa780cc33333e543b741f878098c3b2b142b7cd.

src/General/$.coffee
@@ -53,7 +53,7 @@ $.ajax = do ->
       ) %>
     $('span', message).textContent = (if /^\/\//.test url then location.protocol else '') + url
     new Notice 'error', message, 30, -> delete blockedURLs[url]
-  (url, options, extra={}) ->
+  (url, options={}, extra={}) ->
     {type, whenModified, upCallbacks, form} = extra
     r = new XMLHttpRequest()
     type or= form and 'post' or 'get'