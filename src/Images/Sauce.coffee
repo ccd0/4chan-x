ccd0/4chan-x

commit 06d7b755582c4d9b4f0694995a9d5488aea628c5

Don't run conversions of MD5 hash on every sauce link replacement.

src/Images/Sauce.coffee
@@ -42,19 +42,8 @@ Sauce =
 
     skip = false
     for key of parts
-      parts[key] = parts[key].replace /%(T?URL|IMG|[sh]?MD5|board|name|%|semi)/g, (parameter) ->
-        type = {
-          '%TURL':  post.file.thumbURL
-          '%URL':   post.file.url
-          '%IMG':   if ext in ['gif', 'jpg', 'png'] then post.file.url else post.file.thumbURL
-          '%MD5':   post.file.MD5
-          '%sMD5':  post.file.MD5?.replace /[+/=]/g, (c) -> {'+': '-', '/': '_', '=': ''}[c]
-          '%hMD5':  if post.file.MD5 then ("0#{c.charCodeAt(0).toString(16)}"[-2..] for c in atob post.file.MD5).join('')
-          '%board': post.board.ID
-          '%name':  post.file.name
-          '%%':     '%'
-          '%semi':  ';'
-        }[parameter]
+      parts[key] = parts[key].replace /%(T?URL|IMG|[sh]?MD5|board|name|%|semi)/g, (_, parameter) ->
+        type = Sauce.formatters[parameter] post, ext
         if not type?
           skip = true
           return ''
@@ -86,3 +75,15 @@ Sauce =
       # \u00A0 is nbsp
       nodes.push $.tn('\u00A0'), node
     $.add @file.text, nodes
+
+  formatters:
+    TURL:  (post) -> post.file.thumbURL
+    URL:   (post) -> post.file.url
+    IMG:   (post, ext) -> if ext in ['gif', 'jpg', 'png'] then post.file.url else post.file.thumbURL
+    MD5:   (post) -> post.file.MD5
+    sMD5:  (post) -> post.file.MD5?.replace /[+/=]/g, (c) -> {'+': '-', '/': '_', '=': ''}[c]
+    hMD5:  (post) -> if post.file.MD5 then ("0#{c.charCodeAt(0).toString(16)}"[-2..] for c in atob post.file.MD5).join('')
+    board: (post) -> post.board.ID
+    name:  (post) -> post.file.name
+    '%':   -> '%'
+    semi:  -> ';'

commit 55da327b1073f3e603587b21d76ccc610fc43112

Fix %% and %semi (broken by 06d7b755582c4d9b4f0694995a9d5488aea628c5).

src/Images/Sauce.coffee
@@ -48,7 +48,7 @@ Sauce =
           skip = true
           return ''
 
-        if key is 'url' and parameter isnt '%%' and parameter isnt '%semi'
+        if key is 'url' and parameter not in ['%', 'semi']
           type = JSON.stringify type if /^javascript:/i.test parts['url']
           type = encodeURIComponent type
         type