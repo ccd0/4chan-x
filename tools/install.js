15e928a2782b8e3c0e82d96ec34e54f959c5e901
ccd0 <admin@containerchan.org>
2016-04-12 00:22:08 -0700

    Remove fs-extra.

--- tools/install.js
+++ tools/install.js
@@ -1,8 +1,8 @@
-var fs = require('fs-extra');
+var fs = require('fs');
 
-var installMap = fs.readJsonSync('install.json');
+var installMap = JSON.parse(fs.readFileSync('install.json', 'utf8'));
 for (var src in installMap) {
   for (var dest of installMap[src]) {
-    fs.copySync(src, dest);
+    fs.writeFileSync(dest, fs.readFileSync(src));
   }
 }
