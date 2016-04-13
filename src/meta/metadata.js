2ab00be0b593bbf4535574bfe51cf82556bf2805
ccd0 <admin@containerchan.org>
2016-04-12 23:50:28 -0700

    Fix leftover arguments from converted grunt functions.

--- src/meta/metadata.js
+++ src/meta/metadata.js
@@ -42,5 +42,5 @@
 // @run-at       document-start
 // @updateURL    <%= (channel !== '-noupdate') ? `${meta.downloads}${name}${channel}.meta.js` : 'https://noupdate.invalid/' %>
 // @downloadURL  <%= (channel !== '-noupdate') ? `${meta.downloads}${name}${channel}.user.js` : 'https://noupdate.invalid/' %>
-// @icon         data:image/png;base64,<%= readBase64('src/meta/icon48.png', {encoding: 'base64'}) %>
+// @icon         data:image/png;base64,<%= readBase64('src/meta/icon48.png') %>
 // ==/UserScript==
