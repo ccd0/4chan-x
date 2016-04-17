7233ea4bb3951472013413a44fcd99615b6ed7a3
ccd0 <admin@containerchan.org>
2016-04-17 05:12:38 -0700

    Remove meta.path from package.json.

--- tools/sign.js
+++ tools/sign.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
+var path = require('path');
 var crx = require('crx');
 
 var pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 var channel = process.argv[2] || '';
 
-var privateKey = fs.readFileSync(`../${pkg.meta.path}.keys/${pkg.name}.pem`);
+var privateKey = fs.readFileSync(`../${path.basename(process.cwd())}.keys/${pkg.name}.pem`);
 var archive = fs.readFileSync(`testbuilds/${pkg.name}${channel}.crx.zip`);
 var extension = new crx({privateKey, loaded: true});
 extension.pack(archive).then((data) =>
