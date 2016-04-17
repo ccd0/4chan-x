1e6fe65e51e2d0020e85a41827e54251165de280
ccd0 <admin@containerchan.org>
2016-04-17 15:48:41 -0700

    Check version before trying to upload to store.

--- tools/webstore.js
+++ tools/webstore.js
@@ -1,31 +1,34 @@
 var fs = require('fs');
 var webstore_upload = require('webstore-upload');
+var request = require('request');
 
 var pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+var v = JSON.parse(fs.readFileSync('version.json', 'utf8'));
 var secrets = JSON.parse(fs.readFileSync(`../${pkg.meta.path}.keys/chrome-store.json`, 'utf8'));
 
-webstore_upload({
-  accounts: {
-    default: {
-      publish: true,
-      client_id: secrets.installed.client_id,
-      client_secret: secrets.installed.client_secret,
-    }
-  },
-  extensions: {
-    extension: {
-      appID: pkg.meta.chromeStoreID,
-      zip: `dist/builds/${pkg.name}.zip`
-    }
+request(`https://chrome.google.com/webstore/detail/${pkg.meta.chromeStoreID}`, function (error, response, body) {
+
+  if (body && body.indexOf(`<meta itemprop="version" content="${v.version}" />`) > 0 && process.argv[2] !== 'force') {
+    console.log(`Version ${v.version} already uploaded.`);
+    return;
   }
-}, 'default').catch(function(err) {
-  var code;
-  try {
-    code = JSON.parse(err).more.error.itemError[0].error_code;
-  } catch(err2) {
-  }
-  // PKG_INVALID_VERSION_NUMBER occurs when re-uploading an old version
-  if (code !== 'PKG_INVALID_VERSION_NUMBER') {
+
+  webstore_upload({
+    accounts: {
+      default: {
+        publish: true,
+        client_id: secrets.installed.client_id,
+        client_secret: secrets.installed.client_secret,
+      }
+    },
+    extensions: {
+      extension: {
+        appID: pkg.meta.chromeStoreID,
+        zip: `dist/builds/${pkg.name}.zip`
+      }
+    }
+  }, 'default').catch(function() {
     process.exit(1);
-  }
+  });
+
 });
