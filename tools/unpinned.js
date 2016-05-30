f7c1eed1877280a2bb90eb40af1cffa281637f28
ccd0 <admin@containerchan.org>
2016-05-30 03:17:36 -0700

    Use explicit = for deliberately pinned dependencies.

--- tools/unpinned.js
+++ tools/unpinned.js
@@ -4,7 +4,7 @@ var pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 console.log(
   Object.keys(pkg.devDependencies).filter(
-    name => /^\^/.test(pkg.devDependencies[name])
+    name => !/^=/.test(pkg.devDependencies[name])
   ).map(
     name => `${name}@${process.argv[2] || pkg.devDependencies[name]}`
   ).join(' ')
