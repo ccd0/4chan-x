ccd0/4chan-x

commit 9cee88ce3a9676adb999b6fc2536716a7e8debfb

Put catalog stuff into post rather than moving post stuff into catalog thread.

src/General/Index.coffee
@@ -713,7 +713,9 @@ Index =
     Index.buildReplies threads if Conf['Show Replies']
     nodes = []
     for thread in threads
-      thread.OP.setCatalogOP false
+      if thread.nodes.placeholder
+        $.replace thread.nodes.placeholder, thread.OP.nodes.root
+        delete thread.nodes.placeholder
       if (file = thread.OP.file) and (thumb = thread.OP.file.thumb) and thumb.dataset.src
         thumb.src = thumb.dataset.src
         # XXX https://bugzilla.mozilla.org/show_bug.cgi?id=1021289
@@ -729,7 +731,10 @@ Index =
     Index.sizeCatalogViews threads
     nodes = []
     for thread in threads
-      thread.OP.setCatalogOP true
+      unless thread.nodes.placeholder
+        thread.nodes.placeholder = $.el 'div'
+        $.replace thread.OP.nodes.root, thread.nodes.placeholder
+        $.add thread.catalogView.nodes.root, thread.OP.nodes.root
       nodes.push thread.catalogView.nodes.root
     $.add Index.root, nodes
     if doc.contains Index.root

src/General/Build.coffee
@@ -165,6 +165,7 @@ Build =
   thread: (thread, data) ->
     if (root = thread.nodes.root)
       $.rmAll root
+      delete thread.nodes.placeholder
     else
       thread.nodes.root = root = $.el 'div',
         className: 'thread'
@@ -203,12 +204,11 @@ Build =
     postCount = data.replies + 1
     fileCount = data.images  + !!data.ext
 
+    container = $.el 'div', <%= readHTML('CatalogThread.html') %>
+    $.before thread.OP.nodes.info, [container.childNodes...]
+
     root = $.el 'div',
-      className: 'catalog-thread post' # post added to make 4chan postInfo CSS work
-
-    $.extend root, <%= readHTML('CatalogThread.html') %>
-
-    root.dataset.fullID = thread.fullID
+      className: 'thread catalog-thread'
     $.addClass root, thread.OP.highlights... if thread.OP.highlights
     $.addClass root, 'noFile' unless thread.OP.file
 

src/General/Get.coffee
@@ -18,7 +18,7 @@ Get =
     index = root.dataset.clone
     if index then post.clones[index] else post
   postFromNode: (root) ->
-    Get.postFromRoot $.x '(ancestor::div[contains(@class,"postContainer") or contains(@class,"catalog-thread")][1]|following::div[contains(@class,"postContainer")][1])', root
+    Get.postFromRoot $.x '(ancestor::div[contains(@class,"postContainer")][1]|following::div[contains(@class,"postContainer")][1])', root
   postDataFromLink: (link) ->
     if link.hostname is 'boards.4chan.org'
       path     = link.pathname.split /\/+/

src/Miscellaneous/Nav.coffee
@@ -39,6 +39,7 @@ Nav =
       Nav.scroll +1
 
   getThread: ->
+    return $ '.board' if $.hasClass doc, 'catalog-mode'
     for threadRoot in $$ '.thread'
       thread = Get.threadFromRoot threadRoot
       continue if thread.isHidden and !thread.stub

src/classes/CatalogThread.coffee
@@ -4,11 +4,12 @@ class CatalogThread
   constructor: (root, @thread) ->
     @ID    = @thread.ID
     @board = @thread.board
+    {post} = @thread.OP.nodes
     @nodes =
       root: root
-      thumb:     $ '.catalog-thumb', root
-      icons:     $ '.catalog-icons', root
-      postCount: $ '.post-count',    root
-      fileCount: $ '.file-count',    root
-      pageCount: $ '.page-count',    root
+      thumb:     $ '.catalog-thumb', post
+      icons:     $ '.catalog-icons', post
+      postCount: $ '.post-count',    post
+      fileCount: $ '.file-count',    post
+      pageCount: $ '.page-count',    post
     @thread.catalogView = @

src/classes/Post.Clone.coffee
@@ -7,14 +7,10 @@ Post.Clone = class extends Post
       @[key] = @origin[key]
 
     {nodes} = @origin
-    cloneNode = if contractThumb
-      @cloneWithoutVideo
+    root = if contractThumb
+      @cloneWithoutVideo nodes.root
     else
-      (node) -> node.cloneNode true
-    root = cloneNode nodes.root
-    if @origin.isCatalogOP
-      $.prepend $('.post', root), cloneNode(nodes.fileRoot) if nodes.fileRoot
-      $.add $('.post', root), [cloneNode(nodes.info), cloneNode(nodes.comment)]
+      nodes.root.cloneNode true
     Post.Clone.prefix or= 0
     for node in [root, $$('[id]', root)...]
       node.id = Post.Clone.prefix + node.id
@@ -31,6 +27,10 @@ Post.Clone = class extends Post
     root.hidden = false # post hiding
     $.rmClass root,        'forwarded' # quote inlining
     $.rmClass @nodes.post, 'highlight' # keybind navigation, ID highlighting
+
+    # Remove catalog stuff.
+    $.rm $('.catalog-link', @nodes.post)
+    $.rm $('.catalog-stats', @nodes.post)
 
     @parseQuotes()
     @quotes = [@origin.quotes...]

src/classes/Post.coffee
@@ -36,9 +36,8 @@ class Post
     @parseQuotes()
     @parseFile()
 
-    @isDead      = false
-    @isHidden    = false
-    @isCatalogOP = false
+    @isDead   = false
+    @isHidden = false
 
     @clones = []
     <% if (readJSON('/.tests_enabled')) { %>
@@ -259,11 +258,3 @@ class Post
     for clone in @clones[index..]
       clone.nodes.root.dataset.clone = index++
     return
-
-  setCatalogOP: (isCatalogOP) ->
-    return if @isCatalogOP is isCatalogOP
-    @isCatalogOP = isCatalogOP
-    node = if isCatalogOP then @thread.catalogView.nodes.root else @nodes.post
-    {fileRoot, info, comment} = @nodes
-    $.prepend node, fileRoot if fileRoot
-    $.add node, [info, comment]

src/classes/Thread.coffee
@@ -19,6 +19,7 @@ class Thread
 
     @nodes =
       root: null
+      placeholder: null
 
     @board.threads.push @ID, @
     g.threads.push  @fullID, @

commit 9c30295b8e4cffa03e78594eaf14891536089b5a

Fix shift+click and alt+click in catalog.
Was broken by 9cee88ce3a9676adb999b6fc2536716a7e8debfb which
removed data-full-i-d on catalog threads.

src/General/Index.coffee
@@ -184,9 +184,9 @@ Index =
           true
 
   catalogNode: ->
-    $.on @nodes.root, 'mousedown click', (e) ->
+    $.on @nodes.root, 'mousedown click', (e) =>
       return unless e.button is 0 and e.shiftKey
-      Index.toggleHide g.threads[@dataset.fullID] if e.type is 'click'
+      Index.toggleHide @thread if e.type is 'click'
       e.preventDefault() # Also on mousedown to prevent highlighting text.
 
   toggleHide: (thread) ->

src/Monitoring/ThreadWatcher.coffee
@@ -97,9 +97,9 @@ ThreadWatcher =
 
   catalogNode: ->
     $.addClass @nodes.root, 'watched' if ThreadWatcher.isWatched @thread
-    $.on @nodes.root, 'mousedown click', (e) ->
+    $.on @nodes.root, 'mousedown click', (e) =>
       return unless e.button is 0 and e.altKey
-      ThreadWatcher.toggle g.threads[@dataset.fullID] if e.type is 'click'
+      ThreadWatcher.toggle @thread if e.type is 'click'
       e.preventDefault() # Also on mousedown to prevent highlighting thumbnail in Firefox.
 
   addDialog: ->