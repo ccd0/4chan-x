eb25d6e797a1673fd7cddb257fce04055383ec9b
ccd0 <admin@containerchan.org>
2023-02-26 22:25:54 -0800

    Update chrome-webstore-upload.

--- tools/webstore.js
+++ tools/webstore.js
@@ -7,30 +7,32 @@ var v = JSON.parse(child_process.execSync('git show stable:version.json').toStri
 var secrets = JSON.parse(fs.readFileSync(`../${pkg.meta.path}.keys/chrome-store.json`, 'utf8'));
 var refresh = JSON.parse(fs.readFileSync(`../${pkg.meta.path}.keys/refresh-token.json`, 'utf8'));
 
-var webStore = require('chrome-webstore-upload')({
-  extensionId: pkg.meta.chromeStoreID,
-  clientId: secrets.installed.client_id,
-  clientSecret: secrets.installed.client_secret,
-  refreshToken: refresh.refresh_token
-});
-
-request(`https://chrome.google.com/webstore/detail/${pkg.meta.chromeStoreID}`, function (error, response, body) {
-
-  if (body && body.indexOf(`<meta itemprop="version" content="${v.version}"/>`) > 0 && process.argv[2] !== 'force') {
-    console.log(`Version ${v.version} already uploaded.`);
-    return;
-  }
-
-  var myZipFile = fs.createReadStream(`dist/builds/${pkg.name}.zip`);
-  var token;
-  webStore.fetchToken().then(t => {
-    token = t;
-    return webStore.uploadExisting(myZipFile, token);
-  }).then(() =>
-    webStore.publish()
-  ).catch(res => {
-    console.error(res);
-    process.exit(1);
+import('chrome-webstore-upload').then(chromeWebstoreUpload => {
+  var webStore = chromeWebstoreUpload.default({
+    extensionId: pkg.meta.chromeStoreID,
+    clientId: secrets.installed.client_id,
+    clientSecret: secrets.installed.client_secret,
+    refreshToken: refresh.refresh_token
   });
 
+  request(`https://chrome.google.com/webstore/detail/${pkg.meta.chromeStoreID}`, function (error, response, body) {
+
+    if (body && body.indexOf(`<meta itemprop="version" content="${v.version}"/>`) > 0 && process.argv[2] !== 'force') {
+      console.log(`Version ${v.version} already uploaded.`);
+      return;
+    }
+
+    var myZipFile = fs.createReadStream(`dist/builds/${pkg.name}.zip`);
+    var token;
+    webStore.fetchToken().then(t => {
+      token = t;
+      return webStore.uploadExisting(myZipFile, token);
+    }).then(() =>
+      webStore.publish()
+    ).catch(res => {
+      console.error(res);
+      process.exit(1);
+    });
+
+  });
 });
