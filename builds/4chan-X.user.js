2f111ba533835f446a150f4998734e4e0eb5b324
Matthew Xu <matthew_xu@epam.com>
2026-05-05 11:22:16 -0500

    fix arch.b4k.dev file urls

--- builds/4chan-X.user.js
+++ builds/4chan-X.user.js
@@ -9131,6 +9131,9 @@ Redirect = (function() {
           return '';
         }
       }
+      if (archive.domain === 'arch.b4k.dev') {
+          return "" + (Redirect.protocol(archive)) + "arch-img.b4k.dev" + "/" + boardID + "/" + filename;
+      }
       return "" + (Redirect.protocol(archive)) + archive.domain + "/" + boardID + "/full_image/" + filename;
     },
     board: function(archive, arg) {
